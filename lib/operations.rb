07f1e77d6dd3ef767292463b3c6c73a78c1b49a3
Oscar Alvarez <gavinalv6@gmail.com>
2020-07-30 00:43:25 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,22 @@
-def unsafe?(speed)
 
+speed = 60
+
+def unsafe?(speed)
+  if speed > 60
+    return true
+  elsif speed < 40
+    return true
+  else speed == 50
+    return false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed > 60 ? true : false
+	speed < 40 ? true : false
+	speed == 50 ? false : true
 end
 	
 
